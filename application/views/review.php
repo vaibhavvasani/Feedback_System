769ebb35dc23dff4e02416fef7b0d3c6395ace82
praneeta92115 <praneeta.desai99@gmail.com>
2019-07-11 16:29:46 +0530

    modifications in review UI

--- application/views/review.php
+++ application/views/review.php
@@ -5,100 +5,119 @@ if(!isset($_SESSION['user_id'])){
 }
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Sakec Feedback | Mailbox</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <!-- Bootstrap 3.3.5 -->
-  <link rel="stylesheet" href="<?=base_url();?>bootstrap/css/bootstrap.css">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="<?=base_url();?>icon/fontawesome/css/font-awesome.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="<?=base_url();?>icon/ionicon/css/ionicons.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="<?=base_url();?>dist/css/AdminLTE.min.css">
-  <!-- AdminLTE Skins. Choose a skin from the css/skins
-  folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href="<?=base_url();?>dist/css/skins/_all-skins.min.css">
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
+    <base href="./">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+    <title>KJSCE Feedback System | Student </title>
+    <!-- Icons-->
+    <link href="<?= base_url(); ?>assets/css/coreui-icons.min.css" rel="stylesheet">
+    <link href="<?= base_url(); ?>assets/css/flag-icon.min.css" rel="stylesheet">
+    <link href="<?= base_url(); ?>assets/css/fontawesome.min.css" rel="stylesheet">
+    <link href="<?= base_url(); ?>assets/css/simple-line-icons.css" rel="stylesheet">
+	<link rel="stylesheet" href="<?=base_url();?>bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" href="<?=base_url();?>dist/css/AdminLTE.min.css">
+
+
+    <!-- Main styles for this application-->
+    <link href="<?= base_url(); ?>assets/css/style.css" rel="stylesheet">
+    <link href="<?= base_url(); ?>assets/vendors/pace-progress/css/pace.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- Select2 -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/css/select2.min.css" rel="stylesheet" />
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+
+    
+    <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    // Shared ID
+    gtag('config', 'UA-118965717-3');
+    // Bootstrap ID
+    gtag('config', 'UA-118965717-5');
+    </script>
 </head>
-<body class="hold-transition skin-blue sidebar-mini">
-  <div class="wrapper">
-    <?php $this->load->view('header'); ?>
 
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-      <!-- Content Header (Page header) -->
-      <section class="content-header">
-      </section>
-      <form method="post" action="./review/savereview/">
-        <table>
-          <thead>
-            <tr>
-              <th>NameOfFaculty</th>
-              <th>Review(128 words)</th>
-            </tr>
-          </thead>
-          <tbody>
-            <?php
-            foreach ($rows as $data) {
-              echo "<tr>";
-              echo "<td>$data->NameOfFaculty</td>";
-              echo "<td><textarea required name=\"$data->fid\"></textarea></td>";
-              echo "</tr>";
-            }
-            ?>
-          </tbody>
-        </table>
-        <input type="submit">
-      </form>
-      <!-- /.content -->
-    </div>
-    <!-- /.content-wrapper -->
-    <footer class="main-footer">
-      <div class="pull-right hidden-xs">
-        <b>Version</b> 2.3.2
-      </div>
-      <strong>Copyright &copy; 2015-2016 <a href="http://www.shahandanchor.com">SAKEC(IT dept)</a>.</strong> All rights
-      reserved.
+<body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
+<div class="wrapper">
+    <?php $this->load->view('navbar'); ?>
+    <div class="app-body">
+        <?php $this->load->view('sidebar'); ?>
+        <main class="main" style="overflow: hidden;">
+            <div class="container-fluid mt-4">
+		<div class="content-wrapper pl-5" style="margin-left: 0px;">
+				<!-- Content Header (Page header) -->
+				<section class="content-header">
+				<h1>
+				Feedback Form - Review
+				</h1>
+				</section>
+
+				<!-- Main content -->
+			<section class="content-header">
+			</section>
+			<form method="post" action="./review/savereview/">
+			<table>
+			<thead>
+			<tr>
+			<th>Name of the Faculty</th>
+			<th>Review (max 128 words)</th>
+			</tr>
+			</thead>
+			<tbody>
+			<?php
+			foreach ($rows as $data) {
+			echo "<tr>";
+			echo "<td>$data->NameOfFaculty</td>";
+			echo "<td><textarea required name=\"$data->fid\"></textarea></td>";
+			echo "</tr>";
+			}
+			?>
+			</tbody>
+			</table>
+			<input type="submit">
+			</form>
+			<!-- /.content -->
+			</div>
+			<!-- /.content-wrapper -->
+			</div>
+			<!-- /.content -->
+			</div>
+			</div>
+			</main>
+	<br><br>
+	
+    <footer class="app-footer" style="position: relative; left: 0;  bottom: 0;   width: 100%; padding : 20px;">
+        <div>
+            <a href="#">KJSCE</a>
+            <span>&copy; 2019 All rights reserved.</span>
+        </div>
     </footer>
-
-    <!-- /.control-sidebar -->
-    <!-- Add the sidebar's background. This div must be placed
-    immediately after the control sidebar -->
-    <div class="control-sidebar-bg"></div>
-  </div>
-  <!-- ./wrapper -->
-
-  <!-- jQuery 2.1.4 -->
-  <script src=" <?=base_url();?>plugins/jQuery/jQuery-2.1.4.min.js"></script>
-  <!-- Bootstrap 3.3.5 -->
-  <script src=" <?=base_url();?>bootstrap/js/bootstrap.min.js"></script>
-  <!-- Slimscroll -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.3.0/Chart.bundle.js"></script>
-  <style>
-  canvas {
-    -moz-user-select: none;
-    -webkit-user-select: none;
-    -ms-user-select: none;
-  }
-  .ques {
-    font-size: 20px;
-    word-wrap: break-word;
-  }
-  </style>
-  <script src=" <?=base_url();?>plugins/slimScroll/jquery.slimscroll.min.js"></script>
-  <!-- FastClick -->
-  <script src=" <?=base_url();?>plugins/fastclick/fastclick.js"></script>
-  <!-- AdminLTE App -->
-  <script src=" <?=base_url();?>dist/js/app.min.js"></script>
+    <!-- CoreUI and necessary plugins-->
+    <script src="<?= base_url(); ?>assets/js/jquery-3.4.1.min.js"></script>
+    <script src="<?= base_url(); ?>assets/js/popper.min.js"></script>
+    <script src="<?= base_url(); ?>assets/js/bootstrap.min.js"></script>
+    <script src="<?= base_url(); ?>assets/js/pace.min.js"></script>
+    <script src="<?= base_url(); ?>assets/js/perfect-scrollbar.min.js"></script>
+    <script src="<?= base_url(); ?>assets/js/coreui.min.js"></script>
+    <!-- Plugins and scripts required by this view-->
+    <script src="<?= base_url(); ?>assets/js/Chart.min.js"></script>
+    <script src="<?= base_url(); ?>assets/js/custom-tooltips.min.js"></script>
+    <script src="<?= base_url(); ?>assets/js/main.js"></script>
+    <!-- Select2 -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.7/js/select2.min.js"></script>
+	<script>
 </body>
 </html>
