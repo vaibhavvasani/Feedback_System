0eeb4967f560a2655b04f642aeb8c4773f444548
Vinit <vinitmundra2000@gmail.com>
2019-11-24 12:42:08 +0530

    Added Suchitra Patil Maam

---
+++ Downloads/csi-website-master/README.md
@@ -0,0 +1,10 @@
+# csi-website
+-----
+### URL - PORT:8000
+- / ~homepage
+- /events/
+- /council/
+----
+### ADMIN panel
+- username: csikjsce
+- password: csikjsce
---
+++ Downloads/csi-website-master/Untitled-1.py
@@ -0,0 +1,6 @@
+
+link = input("Enter the link:")
+baselink = "https://drive.google.com/uc?export=download&id="
+new = link.split('/')[5]
+new = baselink+new
+print("New link:",new)
