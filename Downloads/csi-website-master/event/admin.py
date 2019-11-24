20314e61c14c691891eb5f5560c28c09ae22319a
Vinit <vinitmundra2000@gmail.com>
2019-11-24 13:28:23 +0530

    fixed

--- Downloads/csi-website-master/event/admin.py
+++
@@ -1,7 +0,0 @@
-#Import Dependencies
-from django.contrib import admin
-from .models import Event, EventDetail
-
-# Register your models here.
-admin.site.register(Event)
-admin.site.register(EventDetail)
